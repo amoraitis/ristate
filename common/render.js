3e6eade2b8ae84618be70929b68e0e4a990e97bc
Anastasios Moraitis <anas.moraitis@gmail.com>
2023-03-29 08:27:46 +0300

    Update styles

--- common/render.js
+++ common/render.js
@@ -1,10 +1,15 @@
 
 
   document.addEventListener("DOMContentLoaded", function() {
+    
     var baseUrl = window.location.origin + "/common/";
     fetch("https://raw.githubusercontent.com/amoraitis/ristate/main/common/header.html")
       .then(response => response.text())
       .then(html => {
+        if (window.location.origin.indexOf("properties") == -1){
+          
+      
+        }
         document.getElementsByTagName("header")[0].innerHTML = html;
       })
       .then(() => {
