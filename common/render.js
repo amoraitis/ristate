076fcc9b900e4e15293e62f1086123d21a51b9c8
Anastasios Moraitis <anas.moraitis@gmail.com>
2023-03-29 09:26:57 +0300

    Fix headers and add jquery for no reason

--- common/render.js
+++ common/render.js
@@ -1,23 +1,15 @@
-
-
-  document.addEventListener("DOMContentLoaded", function() {
-    
-    var baseUrl = window.location.origin + "/common/";
-    fetch("https://raw.githubusercontent.com/amoraitis/ristate/main/common/header.html")
-      .then(response => response.text())
-      .then(html => {
-        if (window.location.origin.indexOf("properties") == -1){
-          
-      
-        }
-        document.getElementsByTagName("header")[0].innerHTML = html;
-      })
-      .then(() => {
-        fetch("https://raw.githubusercontent.com/amoraitis/ristate/main/common/footer.html")
+$(document).ready(function () {
+  fetch("https://raw.githubusercontent.com/amoraitis/ristate/main/common/header.html")
+    .then(response => response.text())
+    .then(html => {
+      document.getElementsByTagName("header")[0].innerHTML = html;
+    })
+    .then(() => {
+      fetch("https://raw.githubusercontent.com/amoraitis/ristate/main/common/footer.html")
         .then(response => response.text())
         .then(html => {
-            document.getElementsByTagName("footer")[0].innerHTML = html;
+          document.getElementsByTagName("footer")[0].innerHTML = html;
         })
         .then(addEvents);
-      })
-  }, false);
+    })
+});
